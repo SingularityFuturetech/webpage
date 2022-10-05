ee5bb53a8ac1c3d8fd2336860f4c099fda93b0c3
Kenneth Lee <kennethlee@Kenneths-MBP.fios-router.home>
2022-10-05 14:23:54 -0400

    styling products

--- html/service.js
+++ html/service.js
@@ -74,19 +74,16 @@ module.exports = () => `
          </div>
       </div>
    </nav>
-
-      <section class="service-area" id="services" style="background-color: #003366; color:white; margin-top;:40px">
+   <div class="product-head">
+      <div class="col-xs-12">
+         <div class="section-title text-center wow zoomIn" data-wow-duration=".5s" data-wow-delay=".5s">
+            <h2><b>WHAT WE DO</b></h2>
+         </div>
+      </div>
+   </div>
+   <h2 class="product-h2">Our Services</h2>
+   <section class="service-area" id="services" >
          <div class="container">
-            <div class="row">
-               <div class="col-xs-12">
-                  <div class="section-title text-center wow zoomIn" data-wow-duration=".5s" data-wow-delay=".5s">
-                     <h2><b>SERVICES & PRODUCTS</b></h2>
-                     <p>
-                     </p>
-                  </div>
-               </div>
-            </div>
-            <br><br>
             <div class="row">
                <div class="col-md-4 col-sm-6 col-xs-12">
                   <div class="service-wrap text-center">
@@ -106,8 +103,7 @@ module.exports = () => `
                      </div>
                      <h3>Ming Pool</h3>
                      <p>
-                        Earn Bitcoin without self-mining. 
-                     </p>
+                     Earn Bitcoin without doing your own mining.                     </p>
                   </div>
                </div>
                <div class="col-md-4 col-sm-6 col-xs-12">
@@ -124,70 +120,45 @@ module.exports = () => `
             </div>
          </div>
       </section>
-      <section class="teams" id="team">
-         <div class="container text-center wow zoomIn">
-            <h2><b>OUR MINERS</b></h2>
-            <p>
-            <h3>BTC MINERS</h3>
-            </p>
+      <section class="background" id="team">
+         <div>
+         <h2 class="product-h2" style="margin-top:100px">Our BTC Miners</h2>
+         <div class="container-product">
+           <div class="panel pricing-table">
+             <div class="pricing-plan">
+               <img src="img/miner01.png" alt="" class="pricing-img">
+               <h2 class="pricing-header">S99 Max</h2>
+               <ul class="pricing-features">
+                 <li class="pricing-features-item">92th/s Hash Rate</li>
+                 <li class="pricing-features-item">3400w Power Consumption</li>
+                 <li class="pricing-features-item">36j/t Power Efficiency</li>
+               </ul>
+             </div>
+             <div class="pricing-plan">
+               <img src="img/miner02.png" alt="" class="pricing-img">
+               <h2 class="pricing-header">S99 Pro</h2>
+               <ul class="pricing-features">
+               <li class="pricing-features-item">92th/s Hash Rate</li>
+                 <li class="pricing-features-item">3400w Power Consumption</li>
+                 <li class="pricing-features-item">36j/t Power Efficiency</li>
+               </ul>
+             </div>
+             
+             <div class="pricing-plan">
+               <img src="img/miner03.png" alt="" class="pricing-img">
+               <h2 class="pricing-header">S99</h2>
+               <ul class="pricing-features">
+               <li class="pricing-features-item">92th/s Hash Rate</li>
+               <li class="pricing-features-item">3400w Power Consumption</li>
+               <li class="pricing-features-item">36j/t Power Efficiency</li>
+               </ul>
+             </div>
+           </div>
+           <h4 style="text-align:center">contact us for inqury</h4>
          </div>
+       </div>
          <br>
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12">
-                  <div id="team-members" class="owl-carousel owl-theme">
-                     <div class="team-member ">
-                        <img src="img/miner01.png" alt="" class="img-responsive">
-                        <div class="team-member-info text-center">
-                           <h4 class="team-member-name">
-                              S99 MAX.
-                           </h4>
-                           <ul class="social-list">
-                              <li><a href="https://www.instagram.com/thor_miner/" class="social-icon icon-gray"><span class="fab fa-instagram"></span></a></li>
-                              <li><a href="https://www.facebook.com/profile.php?id=100078178533864" class="social-icon icon-gray"><span class="fab fa-facebook"></span></a></li>
-                           </ul>
-                        </div>
-                     </div>
-                     <div class="team-member ">
-                        <img src="img/miner02.png" alt="" class="img-responsive">
-                        <div class="team-member-info text-center">
-                           <h4 class="team-member-name">
-                              S99 PRO
-                           </h4>
-                           <ul class="social-list">
-                              <li><a href="https://www.instagram.com/thor_miner/" class="social-icon icon-gray"><span class="fab fa-instagram"></span></a></li>
-                              <li><a href="https://www.facebook.com/profile.php?id=100078178533864" class="social-icon icon-gray"><span class="fab fa-facebook"></span></a></li>
-                           </ul>
-                        </div>
-                     </div>
-                     <div class="team-member ">
-                        <img src="img/miner03.png" alt="" class="img-responsive" >
-                        <div class="team-member-info text-center">
-                           <h4 class="team-member-name">
-                              S99 S
-                           </h4>
-                           <ul class="social-list">
-                              <li><a href="https://www.instagram.com/thor_miner/" class="social-icon icon-gray"><span class="fab fa-instagram"></span></a></li>
-                              <li><a href="https://www.facebook.com/profile.php?id=100078178533864" class="social-icon icon-gray"><span class="fab fa-facebook"></span></a></li>
-                           </ul>
-                        </div>
-                     </div>
-                     <div class="team-member ">
-                        <img src="img/miner03.png" alt="" class="img-responsive">
-                        <div class="team-member-info text-center">
-                           <h4 class="team-member-name">
-                              S99
-                           </h4>
-                           <ul class="social-list">
-                              <li><a href="https://www.instagram.com/thor_miner/" class="social-icon icon-gray"><span class="fab fa-instagram"></span></a></li>
-                              <li><a href="https://www.facebook.com/profile.php?id=100078178533864" class="social-icon icon-gray"><span class="fab fa-facebook"></span></a></li>
-                           </ul>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
+      </div>
       </section>
       <!------------Counter Starts-------------> 
       <section class="stat " id="stats">
