8aca859af0d949aa114bfe18be18f7c2558c4315
kenneth lee <kennethlee@kenneths-MBP.fios-router.home>
2022-10-27 12:32:28 -0400

    added

--- html/future.js
+++ html/future.js
@@ -5,7 +5,7 @@ module.exports = () => `
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>About Us</title>
+      <title>Future Plan</title>
       <!------------Responsive css file------------->         
       <link href="css/responsive.css" rel="stylesheet" type="text/css">
       <!-------------Magnific popup css file------------->
